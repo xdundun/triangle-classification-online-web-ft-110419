4784fc12fda33c7c328d4c1f88eab06315fcebf2
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-02 03:31:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,19 @@
 class Triangle
   attr_accessor :length1, :length2, :length3
 
-  def initialize(length1 ,length2,length3)
+  def initialize(attributes)
+    attributes.each {|key,value| self.send(("#{key}="),value) }
+
     @length1 = length1
     @length2 = length2
     @length3 = length3
   end
 
   def kind
+
     if @length1 == @length2 && @length2 == @length3
       return :equilateral
+    elsif @length1 == @length2 && @length1 != @length3
     end
   end
 
